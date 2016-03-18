bcf911b265ab96aef00e1d7a7c521f1a2dee1612
LouJia <loujia1991@gmail.com>
2016-03-18 17:35:27 +0800

    [yaxuan/loujia] handle different format amount in bill

--- lib/cash/register/pos.rb
+++ lib/cash/register/pos.rb
@@ -1,10 +1,17 @@
 class Pos
 
   def output(bill, goods_info)
+
     result = ""
+
     for item in bill
-      goods = goods_info[item]
-      result += "名称：" + goods["name"]+"，数量：1，单价："+ goods["price"].to_s + "\n"
+      item_amount = item.split("-")
+      if item_amount[1].nil?
+        item_amount[1] = 1
+      end
+
+      goods = goods_info[item_amount[0]]
+      result += "名称：" + goods["name"]+"，数量：#{item_amount[1]}，单价：#{goods["price"]}" + "\n"
     end
 
     result
--- spec/cash/register_spec.rb
+++ spec/cash/register_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper'
 require 'cash/register/pos'
 require 'cash/register/goods'
+require 'pry'
 
 describe Cash::Register do
 
-  let (:bill) {["ITEM000001", "ITEM000002"]}
-  let (:goods_info) {{
+  let (:goods_info) { {
       "ITEM000001" => {"name" => "apple", "price" => 10},
       "ITEM000002" => {"name" => "banana", "price" => 6},
-  }}
-  let (:pos) {Pos.new}
+  } }
+  let (:pos) { Pos.new }
 
-  it 'should return the receipt which contains name and price' do
-    receipt = pos.output(bill, goods_info)
-    expect(receipt).to eql("名称：apple，数量：1，单价：10"+ "\n" + "名称：banana，数量：1，单价：6" + "\n" )
+  context "simple bill" do
+    let (:bill) { ["ITEM000001", "ITEM000002"] }
+
+    it 'should return the receipt which contains name and price' do
+      receipt = pos.output(bill, goods_info)
+      expect(receipt).to eql("名称：apple，数量：1，单价：10"+ "\n" + "名称：banana，数量：1，单价：6" + "\n")
+    end
+  end
+
+  context "simple bill with mutiple format items" do
+    let (:bill) { ["ITEM000001-2", "ITEM000002"] }
+
+    it 'should return the receipt which contains amount' do
+      receipt = pos.output(bill, goods_info)
+
+      expect(receipt).to eql("名称：apple，数量：2，单价：10"+ "\n" + "名称：banana，数量：1，单价：6" + "\n")
+    end
   end
 
 end
