75752822aba3e59eaaecfa90ca2fbd082858656c
LouJia <loujia1991@gmail.com>
2016-03-23 16:21:35 +0800

    calculate total money and print it in receipt

--- lib/cash/register/pos.rb
+++ lib/cash/register/pos.rb
@@ -2,16 +2,21 @@ require 'cash/register/models/goods'
 require 'cash/register/models/item'
 
 class Pos
+  HEADER = "***<没钱赚商店>购物清单***"
+  FOOTER = "**********************"
 
   def printReceipt(bill, goods_info)
     item_list = parseBill(bill, goods_info)
 
-    result = ""
+    result = HEADER + "\n"
+    money=0
     item_list.each {|item|
          result += "名称：#{item.name}，数量：#{item.amount}，单价：#{item.price}(元)，小计：#{item.cost}(元)" + "\n"
+      money += item.cost
     }
+    result += "总计: #{money}(元)" + "\n"
+    result += FOOTER
 
-    result
   end
 
   def parseBill(bill, goods_info)
@@ -19,15 +24,19 @@ class Pos
 
     for line in bill
       item_amount = line.split("-")
-      if item_amount[1].nil?
-        item_amount[1] = 1
-      end
+      getAmount(item_amount)
 
       goods = goods_info[item_amount[0]]
       cost = calculateMoney(goods["price"], item_amount[1])
       item_list << Item.new(goods["name"], item_amount[1], goods["price"], cost)
     end
     item_list
+  end
+
+  def getAmount(item_amount)
+    if item_amount[1].nil?
+      item_amount[1] = 1
+    end
   end
 
   def calculateMoney(price, amount)
--- spec/cash/register_spec.rb
+++ spec/cash/register_spec.rb
@@ -11,22 +11,16 @@ describe Cash::Register do
   } }
   let (:pos) { Pos.new }
 
-  context "simple bill" do
-    let (:bill) { ["ITEM000001", "ITEM000002"] }
-
-    it 'should return the receipt which contains name and price' do
-      receipt = pos.printReceipt(bill, goods_info)
-      expect(receipt).to eql("名称：apple，数量：1，单价：10(元)，小计：10(元)"+ "\n" + "名称：banana，数量：1，单价：6(元)，小计：6(元)" + "\n")
-    end
-  end
-
   context "simple bill with mutiple format items" do
     let (:bill) { ["ITEM000001-2", "ITEM000002"] }
 
     it 'should return the receipt which contains amount' do
       receipt = pos.printReceipt(bill, goods_info)
 
-      expect(receipt).to eql("名称：apple，数量：2，单价：10(元)，小计：20(元)"+ "\n" + "名称：banana，数量：1，单价：6(元)，小计：6(元)" + "\n")
+      expect(receipt).to eql( "***<没钱赚商店>购物清单***" + "\n"  +
+                            "名称：apple，数量：2，单价：10(元)，小计：20(元)"+ "\n" + "名称：banana，数量：1，单价：6(元)，小计：6(元)" + "\n" +
+                             "总计: 26(元)" + "\n" +
+                            "**********************")
     end
   end
 
